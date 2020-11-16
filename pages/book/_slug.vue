001f1ed1ae6173c1d0862c2bcbd07cdc6d8c2734
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2020-11-16 00:41:23 -0500

    first

---
+++ pages/book/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      Individual Book Here
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/books.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>Welcome To The Book Page</h1>
+      <div v-for="book in books" :key="book.id">
+        <div  v-html="book.excerpt.rendered"/>
+       <NuxtLink :to="`/book/${book.slug}`"> {{book.title.rendered}} </NuxtLink>
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+  data: () => ({
+    books: []
+  }),
+  async fetch() {
+    const response = await this.$axios.get('/wp-json/wp/v2/books')
+    this.books = response.data
+  },
+  fetchOnServer: true
+}
+</script>
+
+<style>
+
+</style>
